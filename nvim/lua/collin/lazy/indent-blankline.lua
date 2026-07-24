f47ca0ed3f34b65b6bfbd15d2e6ad3468af9b44e
Collin Robison <collinrobison@gmail.com>
2026-07-24 16:16:39 -0500

    add indent blankline

---
+++ nvim/lua/collin/lazy/indent-blankline.lua
@@ -0,0 +1,34 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    { "<leader>ui", "<cmd>IBLToggle<CR>", desc = "Toggle indent guides" },
+    { "<leader>us", "<cmd>IBLToggleScope<CR>", desc = "Toggle indent scope" },
+  },
+  config = function()
+    local hooks = require("ibl.hooks")
+
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      local colors = require("vague").get_palette()
+      vim.api.nvim_set_hl(0, "IblIndentMuted", { fg = colors.line })
+      vim.api.nvim_set_hl(0, "IblScopePurple", { fg = colors.parameter, underline = true })
+    end)
+
+    require("ibl").setup({
+      indent = {
+        char = "▏",
+        highlight = "IblIndentMuted",
+      },
+      whitespace = {
+        remove_blankline_trail = true,
+      },
+      scope = {
+        char = "▎",
+        highlight = "IblScopePurple",
+        show_start = true,
+        show_end = false,
+      },
+    })
+  end,
+}
