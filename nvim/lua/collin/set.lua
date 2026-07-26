13897757256ebfca0e157388290e1a332c2d5708
Collin Robison <collinrobison@gmail.com>
2026-07-26 00:48:25 -0500

    added lazy git

--- nvim/lua/collin/set.lua
+++ nvim/lua/collin/set.lua
@@ -5,6 +5,19 @@ vim.cmd.colorscheme("vague")
 -- Enable 24-bit GUI colors for themes and UI plugins.
 opt.termguicolors = true
 
+if vim.fn.executable("nvr") == 1 then
+  local servername = vim.v.servername
+  if servername == "" then
+    servername = vim.fn.serverstart(vim.fn.stdpath("run") .. "/nvim-" .. vim.fn.getpid() .. ".sock")
+  end
+
+  vim.env.NVIM_LISTEN_ADDRESS = servername
+  local nvr = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+  vim.env.EDITOR = nvr
+  vim.env.GIT_EDITOR = nvr
+  vim.env.VISUAL = nvr
+end
+
 -- Persist buffers, layout, working directory, terminals, and local options in sessions.
 opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
@@ -30,4 +43,3 @@ opt.cursorline = true -- highlight the current cursor line
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
-
